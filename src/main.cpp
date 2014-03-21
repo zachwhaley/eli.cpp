bbad1ef6aa22d1bd9e0c789ad590e58ab9930cfe
Zach Whaley <zachbwhaley@gmail.com>
2014-03-20 20:39:25 -0500

    Added slurp function
    slurp opens a file and reads the contents of the file into a string, then returns that string.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,32 @@
+#include <fstream>
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+string slurp(const string &filename)
+{
+    ifstream ifs{filename, ios::in | ios::binary | ios::ate};
+    if (ifs)
+    {
+        string str;
+
+        ifs.seekg(0, ios::end);
+        str.resize(ifs.tellg());
+        ifs.seekg(0, ios::beg);
+
+        ifs.read(&str.front(), str.size());
+
+        return str;
+    }
+    return "";
+}
 
 int main(int argc, char *argv[])
 {
     if (argc > 1)
     {
-        cout << argv[1] << endl;
+        cout << slurp(argv[1]) << endl;
     }
     else
     {
