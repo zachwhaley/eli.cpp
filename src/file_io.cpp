aeda231d7dabb0896e630b0d8f62773644e6657e
Zachary Whaley <zachbwhaley@gmail.com>
2015-01-29 07:47:08 -0600

    Do not throw exceptions in file_io

--- src/file_io.cpp
+++ src/file_io.cpp
@@ -8,19 +8,16 @@ namespace editor
 {
     string slurp(const string &filename)
     {
-        ifstream ifs{filename, ios::in | ios::binary | ios::ate};
+        string str;
+        ifstream ifs {filename, ios::in | ios::binary | ios::ate};
         if (ifs) {
-            string str;
-
             ifs.seekg(0, ios::end);
             str.resize(ifs.tellg());
             ifs.seekg(0, ios::beg);
 
             ifs.read(&str.front(), str.size());
-
-            return str;
         }
-        throw;
+        return str;
     }
 
 } // namespace editor
