e100e5899f3da6ab38fa4aa0b9a2add19ad115f3
Zachary Whaley <zachbwhaley@gmail.com>
2015-01-29 07:42:15 -0600

    remove superfluous constructors from window

--- src/window.hpp
+++ src/window.hpp
@@ -8,15 +8,6 @@ namespace editor
     class Window
     {
     public:
-        /* Copy Contructor */
-        Window(const Window &) = delete;
-        /* Move Contructor */
-        Window(Window &&) = default;
-        /* Copy Assignment Operator */
-        Window& operator=(const Window &) = delete;
-        /* Move Assignment Operator */
-        Window& operator=(Window &&) = default;
-
         /* Constructor */
         Window(int height, int width, int start_y, int start_x);
         /* Destructor */
